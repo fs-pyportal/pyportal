06d0bb5286df30aa160f79676b4316f5fd9876c2
Davit <davit.babayan@outlook.com>
2023-03-06 22:40:54 +0400

    Base components

--- core/service/storage.py
+++ core/service/storage.py
@@ -1,6 +1,33 @@
-from core.service.storage_abstruct import StorageAbstruct
+from core.internal.errors import MethodNotImplemented
 
-class Storage(StorageAbstruct):
+class Storage:
     '''
     Wrapper for interact to actual storage
     '''
+
+    def __init__():
+        raise MethodNotImplemented
+
+    def connect(self, credential):
+        raise MethodNotImplemented
+
+    def upload(self, content, name):
+        raise MethodNotImplemented
+
+    def download(fself, file_id):
+        raise MethodNotImplemented
+
+    def stat(self, file_id):
+        raise MethodNotImplemented
+
+    def rename(self, file_id, new_name):
+        raise MethodNotImplemented
+
+    def delete(self, file_id):
+        raise MethodNotImplemented
+
+    def set_attribute(self, file_id, name, value):
+        raise MethodNotImplemented
+
+    def get_attribute(self, file_id, name):
+        raise MethodNotImplemented
